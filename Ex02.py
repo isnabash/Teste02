c5b67a3058f379a328e7828399cb310362fffec3
Pharaoh <isnabanm@gmail.com>
2022-03-15 22:30:52 -0300

    teste inicial

---
+++ Ex02.py
@@ -0,0 +1,5 @@
+distancia_Milhas = float(input("Digite uma distância em Milhas: "))
+
+distancia_KM = 1.61 * distancia_Milhas
+
+print(f"A distância em Kilometros é: {distancia_KM:.0f}")
---
+++ Ex03.py
@@ -0,0 +1,5 @@
+distancia_Km = float(input("Digite a distância em Km: "))
+
+distancia_Milhas = distancia_Km / 1.61
+
+print(f"A distancia em Milhas é: {distancia_Milhas:.0f}")
